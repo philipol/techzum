106c58a08db018ea4bcdae9ba85419a5cb54285b
philipol <56820000+philipol@users.noreply.github.com>
2021-06-07 15:38:27 +0200

    added app.js, index.js and styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Index from "./Pages/Index";
+import Services from "./Pages/Services";
+import { Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Route path="/" exact component={Index} />
+      <Route path="/vad-vi-gor" exact component={Services} />
     </div>
   );
 }
